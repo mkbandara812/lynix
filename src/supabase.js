e354c5072868c51eefb751750417368bcfeb2471
mkbandara812 <111906483+mkbandara812@users.noreply.github.com>
2026-04-01 20:28:50 +0530

    Delete src directory

--- src/supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(
-  supabaseUrl || 'https://ppredshlgdmxvttwwjef.supabase.co', 
-  supabaseAnonKey || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwcmVkc2hsZ2RteHZ0dHd3amVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3OTc0NzYsImV4cCI6MjA5MDM3MzQ3Nn0.eglt9ui-aO04sWBsZnHr0e8EztGOfeZq_C72gMTjg3Q'
-);
